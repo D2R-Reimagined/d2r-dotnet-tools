2d525674d896ac8e01dab11eec5ecf512889224a
Collin Herber <rustaddicts@gmail.com>
2025-04-08 18:44:35 -0400

    feat: update cubemain parser to more-better dynamic fetching parser

---
+++ FileExtensions/Extensions/ColumnExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace D2RReimaginedTools.Extensions;
+
+public static class ColumnExtensions
+{
+    public static string GetValue(this string[] columns, Dictionary<string, int> columnMap, string columnName)
+    {
+        return columnMap.TryGetValue(columnName, out var index) && index < columns.Length
+            ? columns[index]
+            : string.Empty;
+    }
+}
